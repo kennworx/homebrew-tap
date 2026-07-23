e03aeb83ab15916360a524ba14a759a433b8d987
kenn-release <kenn-release@users.noreply.github.com>
2026-07-23 12:10:29 +0000

    kenn-swift 0.2.1

--- Formula/kenn-swift.rb
+++ Formula/kenn-swift.rb
@@ -1,13 +1,13 @@
 class KennSwift < Formula
   desc "kenn's Swift indexer"
   homepage "https://github.com/kennworx/kenn"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT OR Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/kennworx/kenn/releases/download/v0.2.0/kenn-swift-aarch64-apple-darwin.tar.gz"
-      sha256 "f214c04a81a7d17678b022df4d3bccde362a42f95c0444a7347f9ac6dc5f4a5d"
+      url "https://github.com/kennworx/kenn/releases/download/v0.2.1/kenn-swift-aarch64-apple-darwin.tar.gz"
+      sha256 "1a3533f5093d0186cef45b8a670df51d3406164be4c0cb3388887edbad4fbb98"
     end
   end
 
