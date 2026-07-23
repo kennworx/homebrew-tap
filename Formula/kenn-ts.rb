138ed5c71d32efe46de3a5e10c35b86c573601ac
kenn-release <kenn-release@users.noreply.github.com>
2026-07-23 12:10:40 +0000

    kenn-ts 0.2.1

--- Formula/kenn-ts.rb
+++ Formula/kenn-ts.rb
@@ -1,24 +1,24 @@
 class KennTs < Formula
   desc "kenn's TypeScript/JavaScript indexer"
   homepage "https://github.com/kennworx/kenn"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT OR Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/kennworx/kenn/releases/download/v0.2.0/kenn-ts-aarch64-apple-darwin.tar.gz"
-      sha256 "bd467684238000bf8a6ebc2156b068aca4a1ca76755c0b6b09dd19756dc096d8"
+      url "https://github.com/kennworx/kenn/releases/download/v0.2.1/kenn-ts-aarch64-apple-darwin.tar.gz"
+      sha256 "383cf28c72f33263b5f547ec0dc5c161aaa535fa55f9a0af798f6bb88559b204"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/kennworx/kenn/releases/download/v0.2.0/kenn-ts-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "3ba7e27e3e1a37edd53a955b63917e086dc1fdc152fec420302bfa025667daa6"
+      url "https://github.com/kennworx/kenn/releases/download/v0.2.1/kenn-ts-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "3788b2c66617aee4a1f1eb701bff8a0211fbe6804ada49e1e6bcbd0f5c803ba9"
     end
     on_intel do
-      url "https://github.com/kennworx/kenn/releases/download/v0.2.0/kenn-ts-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "32ffd5bfc0a77bc9b16fe710d0b6bb9811f3c143cff19908a07f06d223a3d7e2"
+      url "https://github.com/kennworx/kenn/releases/download/v0.2.1/kenn-ts-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "8534eab24905be8fbf0662cebe932640a717ad1418b79779429884a87b314307"
     end
   end
 
